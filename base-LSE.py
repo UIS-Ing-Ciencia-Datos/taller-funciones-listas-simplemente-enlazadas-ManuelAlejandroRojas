88346019023c8a2aa1c7caa3286a2e5f24bc1d5f
ManuelAlejandroRojas <manuel2250938@correo.uis.edu.co>
2026-02-17 18:31:42 -0500

    Add files via upload

--- base-LSE.py
+++ base-LSE.py
@@ -30,3 +30,37 @@ class ListaSE:
 		else:
 			nuevo_nodo.siguiente = self.cabeza
 			self.cabeza = nuevo_nodo
+
+
+
+	#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	#Manuel Rojas Changes
+	#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+	#Contar cuantos elementos tiene la lista
+	def Contar_cuantos_elementos_tiene_la_lista(self):
+		CantidadElementos = 0
+		NodoActual = self.cabeza
+		while NodoActual != None:
+			CantidadElementos+=1
+			NodoActual = NodoActual.siguiente
+		
+		print(CantidadElementos)
+
+
+	#Encontrar un elemento por su valor
+	def Buscar_si_hay_un_elemento(self, valor):
+		NodoActual = self.cabeza
+		while NodoActual != None:
+			if NodoActual == valor:
+				print("El valor si está en la lista --> Verdadero")
+				return True
+			
+			else:
+				NodoActual = NodoActual.siguiente
+
+			if NodoActual == None:
+				print("Falso, el valor no está en la lista")
+				return False
+			
+
